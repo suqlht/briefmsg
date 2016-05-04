4f82211cd93680e05b83c1024577e929f5260e6e
liuhaitao <suqlht@163.com>
2016-05-05 06:00:38 +0800

    fsstat

--- userlib/filelist.js
+++ userlib/filelist.js
@@ -2,12 +2,32 @@
  * http://usejsdoc.org/
  */
 var fs = require('fs');
-exports.getFilelists = function(path,callback){
-fs.readdir(path, function(err, files){	
+var path = require('path');
+
+exports.getFilelists = function(path_dir,callback){
+
+fs.readdir(path_dir, function(err, files){	
+		var filelist = [];
 	if(err){
 		console.log(err);
 		files=[]};
-   callback(files); 	
+    files.forEach(function(file){
+    	var stats = fs.statSync(path.join(path_dir,file));
+    		if(stats){
+    			
+    			var fileobject = {};
+    			fileobject.name = file;
+    			fileobject.size = stats.size;
+    			console.log(fileobject);
+
+    			filelist.push(fileobject);
+    				}   			
+    
+    });
+ console.log(filelist);
+   callback(filelist); 	
 }
 )};
+
+
 
